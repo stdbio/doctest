8f65488b2c3dd1df43980278e71030aba4a8486d
Egor Suvorov <egor.suvorov@gmail.com>
2022-03-05 15:36:58 +0300

    Allow escaping backslash with backslash in filters (#614) (#616)
    * Allow escaping backslash with backslash in filters (#614)
    
    * Add test
    
    * Strengthen test conditions
    
    Co-authored-by: Salvage <29021710+Saalvage@users.noreply.github.com>

--- doctest/parts/doctest.cpp
+++ doctest/parts/doctest.cpp
@@ -3379,8 +3379,8 @@ namespace {
                 char character = *current++;
                 if(seenBackslash) {
                     seenBackslash = false;
-                    if(character == ',') {
-                        s.put(',');
+                    if(character == ',' || character == '\\') {
+                        s.put(character);
                         continue;
                     }
                     s.put('\\');
